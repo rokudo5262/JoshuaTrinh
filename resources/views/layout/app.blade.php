2acd4d6487d0304a77f825950c90300d26cb1429
JoshuaTrinh <rokudo5262@gmai.com>
2022-04-08 14:48:26 +0700

    install vue.js 2

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,8 +6,9 @@
         <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('css/post.css') }}" />
         <meta name="csrf-token" content="{{ csrf_token() }}" />
         <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous">
-        <script type="text/javascript"  src="{{ asset('js/user.js') }}"></script>
-        <script type="text/javascript"  src="{{ asset('js/post.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('js/user.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('js/post.js') }}"></script>
+        <script src="{{ mix('/js/app.js') }}"></script>
     </script>
     </head>
     <body>
